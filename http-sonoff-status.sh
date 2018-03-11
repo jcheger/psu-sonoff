60065a51f382cd308cf366d886e605f94de5f7c4
Jean-Christophe Heger <jcheger@ordinoscope.net>
2018-03-11 17:21:23 +0100

    status can be boolean or text

--- http-sonoff-status.sh
+++ http-sonoff-status.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-wget -O - -q "http://$1/cm?user=$2&password=$3&cmnd=Power1" | grep '"POWER":"1"'
+OUT=`wget -O - -q "http://$1/cm?user=$2&password=$3&cmnd=Power1" | sed 's/^.*:"//g' | sed 's/".$//'`
 
-if [ $? -eq 0 ]; then
+if [ "$OUT" = "ON" ]; then
 	echo "ON"
 	exit 0
-else
-	echo "OFF"
-	exit 1
 fi
+
+if [ "$OUT" = "1" ]; then
+	echo "ON"
+	exit 0
+fi
+
+
+echo "OFF"
+exit 1
